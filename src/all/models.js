bb665416c8f608a9f7ce347d5c0c4327d2f82d87
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-03 09:01:10 +0200

    Adds models.js

---
+++ src/all/models.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import Club from '../Club';
+import Competition from '../Competition';
+import LineUp from '../LineUp';
+import LineUpMember from '../LineUpMember';
+import Location from '../Location';
+import Match from '../Match';
+import Model from '../Model';
+import Person from '../Person';
+import Player from '../Player';
+import Round from '../Round';
+import Season from '../Season';
+import Section from '../Section';
+import Sport from '../Sport';
+import Team from '../Team';
+import TeamMember from '../TeamMember';
+
+
+export default Model;
+export {
+	Club,
+	Competition,
+	LineUp,
+	LineUpMember,
+	Location,
+	Match,
+	Model,
+	Person,
+	Player,
+	Round,
+	Season,
+	Section,
+	Sport,
+	Team,
+	TeamMember
+};
